13ff498b36544d539a210501f2d91ac48eb3fa40
Pranay Kanwar <pranay.kanwar@gmail.com>
2021-12-08 12:56:13 +0530

    Bump jmx-dump version.

--- jmx-dump.rb
+++ jmx-dump.rb
@@ -1,8 +1,8 @@
 class JmxDump < Formula
   desc "Dump JMX Metrics on command line."
   homepage "https://github.com/r4um/jmx-dump"
-  url "https://github.com/r4um/jmx-dump/releases/download/0.10.3/jmx-dump-0.10.3-standalone.jar"
-  sha256 "4f49f92dc33229b3fbad882302a0aff1046e0a60db57a8894052fbd41ccce08c"
+  url "https://github.com/r4um/jmx-dump/releases/download/0.11.3/jmx-dump-0.11.3-standalone.jar"
+  sha256 "8640c6061e63ea9c4dc81af4808d4387de2c4c4b0c5c458f46fffd6efc90dc06"
 
   depends_on "openjdk"
 
